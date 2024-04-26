8036bbe49a08c45f299e5bdd4434a8aa4e8904be
Viacheslav Luchytskyi <viacheslav.luchytskyi@medeanalytics.com>
2024-04-26 21:59:15 +0300

    Added new source code data to drawer

--- reqs-client/src/App.tsx
+++ reqs-client/src/App.tsx
@@ -127,8 +127,8 @@ export function App() {
                     size={ "default" }>
                 <Tabs className={ "tabs-source-code" } id={ "code" }>
                     <Tab title={ "UML" } id={ "uml" } panel={ <Pre className="code-block" children={ selectedDiagram?.uml }/> } />
-                    <Tab title={ "TOKENS" } id={ "tkn" } panel={ <Pre className={"code-block"} children={ selectedDiagram?.tokens }/> } />
-                    <Tab title={ "DATA" } id={ "data" } panel={ <Pre className={"code-block"} children={ selectedDiagram?.dataSetDto }/> } />
+                    <Tab title={ "TOKENS" } id={ "tkn" } panel={ <Pre className="code-block" children={ selectedDiagram?.tokens }/> } />
+                    <Tab title={ "DATA" } id={ "data" } panel={ <Pre dangerouslySetInnerHTML={{ __html: syntaxHighlight(selectedDiagram?.dataSetDto)}}/>} />
                 </Tabs>
             </Drawer>
             <Overlay2 isOpen={ isLoading }>
@@ -142,3 +142,28 @@ export function App() {
 
 
 export default App
+
+function syntaxHighlight(json: any | null) {
+    if (json == null) {
+        return undefined;
+    }
+    if (typeof json != 'string') {
+        json = JSON.stringify(json, undefined, 2);
+    }
+    json = json.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;');
+    return json.replace(/("(\\u[a-zA-Z0-9]{4}|\\[^u]|[^\\"])*"(\s*:)?|\b(true|false|null)\b|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?)/g, function (match: string) {
+        var cls = 'number';
+        if (/^"/.test(match)) {
+            if (/:$/.test(match)) {
+                cls = 'key';
+            } else {
+                cls = 'string';
+            }
+        } else if (/true|false/.test(match)) {
+            cls = 'boolean';
+        } else if (/null/.test(match)) {
+            cls = 'null';
+        }
+        return '<span class="' + cls + '">' + match + '</span>';
+    });
+}
